cd81ec4410233fd9372c44cf5223c932e0db2a75
sunsunarto <sunaryo24259@outlook.com>
2024-11-15 13:18:13 +0700

    update

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './component/header.jsx'
 import Sidebar from './component/sidebar.jsx'
+import MiddleComponent from './component/middleComponent.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header/>
       <Sidebar/>
+      <MiddleComponent/>
     </>
   )
 }
--- project/src/component/middleComponent.jsx
+++ project/src/component/middleComponent.jsx
@@ -1,11 +1,13 @@
 import './styleMiddleComponent.css'
-
-const middleComponent = () => {
+import logoK from './assets/Screenshot_2024-11-15_124027-removebg-preview.png'
+const MiddleComponent = () => {
     return(
         <body>
             <div className="logo">
-                
+                <img src={logoK} alt="" />
+                <textarea cols={50}> what happend today?</textarea>
             </div>
         </body>
     )
 }
+export default MiddleComponent
