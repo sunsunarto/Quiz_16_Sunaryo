ebd63bf79ac6f40b1356834ed0653e7bea893b65
sunsunarto <sunaryo24259@outlook.com>
2024-11-15 12:31:02 +0700

    Update middleComponent.jsx

---
+++ project/src/component/middleComponent.jsx
@@ -0,0 +1,11 @@
+import './styleMiddleComponent.css'
+
+const middleComponent = () => {
+    return(
+        <body>
+            <div className="logo">
+                
+            </div>
+        </body>
+    )
+}
