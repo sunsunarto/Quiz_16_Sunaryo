76453ef8609f133c742f72609c84027d7dc9787b
sunsunarto <sunaryo24259@outlook.com>
2024-11-16 21:11:26 +0700

    add icon

--- project/src/component/topMiddleComponent.jsx
+++ project/src/component/topMiddleComponent.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import logoK from './assets/Screenshot_2024-11-15_124027-removebg-preview.png';
 import './styleTopMiddleComponent.css'
-
+import iconImage from './assets/image-icon-free-png.png'
 const TopMiddleComponent = () => {
     const [text, setText] = useState('');
     const [limit, setLimit] = useState(150); // Set the word limit here
@@ -15,13 +15,22 @@ const TopMiddleComponent = () => {
     }
 
     return (
-        <div className="logo">
-            <img src={logoK} alt="" /> 
-            <div className="textArea">
-                <textarea value={text} onChange={handleChange} cols={50} rows={2} placeholder={`You can input up to ${limit} words`} />
-                <p>{text.length}/150</p>
+            <div className="topMiddlecontainer">
+                <div className="logoAndtextarea">
+                    <div className="logo">
+                        <img src={logoK} alt="" /> 
+                    </div>
+                    <div className="textArea">
+                        <textarea value={text} onChange={handleChange} cols={50} rows={2} placeholder={`You can input up to ${limit} words`} />
+                        <p>{text.length}/150</p>
+                    </div>
+                </div>
+                <div className="iconGroup">
+                    <div className="icons">
+                        <img src={iconImage} alt="" />
+                    </div>
+                </div>
             </div>
-        </div>
     );
 };
 
